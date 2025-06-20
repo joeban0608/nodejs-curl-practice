b01b84e4dc2917a1e2ad7733a8b37d694337419e
joeban <joeanddaneil5@gmail.com>
2025-06-20 13:09:07 +0800

    - 改成 busboy stream 寫法 - busboy response file data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path"); // ✅ 加上這行
 const multer = require("multer");
+const busboy = require("busboy");
 
 const uploadDir = path.join(__dirname, "uploads");
 if (!fs.existsSync(uploadDir)) {
@@ -9,21 +10,21 @@ if (!fs.existsSync(uploadDir)) {
 }
 
 const app = express();
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/");
-  },
-  filename: function (req, file, cb) {
-    const reqName = req?.body?.name;
-    const ext = path.extname(file.originalname); // etc .png, .jpg, .mov
-    const formatFileName = reqName
-      ? `${Date.now()}-${reqName}${ext}`
-      : Date.now() + ext; // Use the name from the request body if available
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "uploads/");
+//   },
+//   filename: function (req, file, cb) {
+//     const reqName = req?.body?.name;
+//     const ext = path.extname(file.originalname); // etc .png, .jpg, .mov
+//     const formatFileName = reqName
+//       ? `${Date.now()}-${reqName}${ext}`
+//       : Date.now() + ext; // Use the name from the request body if available
 
-    cb(null, formatFileName);
-  },
-});
-const upload = multer({ storage });
+//     cb(null, formatFileName);
+//   },
+// });
+// const upload = multer({ storage });
 
 const port = 3000;
 // Middleware to parse JSON requests
@@ -67,39 +68,99 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
  * 對應   curl -F "filepath" 的keyname
  * image example:
  * - curl -F filepath=@/Users/hongbangzhou/Downloads/joebanV1.png http://localhost:3000/upload
- * - curl -F name=testName -F filepath=@/Users/hongbangzhou/Downloads/joebanV1.png http://localhost:3000/upload
+ * - curl -F name=testName -F description="testDescription" -F filepath=@/Users/hongbangzhou/Downloads/joebanV1.png http://localhost:3000/upload
  * video example:
  * - curl -F filepath=@/Users/hongbangzhou/Downloads/screenshot.mov http://localhost:3000/upload
  * else example:
  * - curl -F name=joeban -F shoesize=11  http://localhost:3000/upload
  */
-app.post("/upload", upload.single("filepath"), (req, res) => {
+app.post("/upload", (req, res) => {
   const requestData = {
     method: req.method,
     url: req.url,
     headers: req.headers,
     body: req.body,
-    file: req.file,
   };
   console.log("requestData:", requestData);
-  if (!req.file) {
-    return res.status(400).json({ error: "No file uploaded" });
-  }
+  const bb = busboy({ headers: req.headers });
+  let reqName;
+  let resFile = {};
+  let totalSize = 0;
 
-  const fileUrl = `${req.protocol}://${req.get("host")}/uploads/${
-    req.file.filename
-  }`;
-
-  res.json({
-    message: "File upload successful",
-    file: {
-      originalName: req.file.originalname,
-      mimeType: req.file.mimetype,
-      size: req.file.size,
-      url: fileUrl,
-      // file: req.file,
-    },
+  /**
+   * form fields
+   * example output:
+   * field: {
+      "name": "name",
+      "val": "testName",
+      "info": {
+        "nameTruncated": false,
+        "valueTruncated": false,
+        "encoding": "7bit",
+        "mimeType": "text/plain"
+      }
+    }
+   * field: {
+      "name": "description",
+      "val": "testDescription",
+      "info": {
+        "nameTruncated": false,
+        "valueTruncated": false,
+        "encoding": "7bit",
+        "mimeType": "text/plain"
+      }
+    }
+   */
+  bb.on("field", (name, val, info) => {
+    // console.log("field:", JSON.stringify({ name, val, info }, null, 2));
+    if (name === "name" && val) {
+      reqName = val;
+    }
   });
+  bb.on("file", (_name, file, info) => {
+    const formatName = reqName
+      ? `${Date.now()}-${reqName}${path.extname(info.filename)}`
+      : `${Date.now()}-${info.filename}`; // Use the name from the request body if available
+    const saveTo = path.join(uploadDir, formatName);
+    const fileUrl = `${req.protocol}://${req.get(
+      "host"
+    )}/uploads/${formatName}`;
+
+    resFile.url = fileUrl; // Set the URL of the uploaded file
+    if (reqName) {
+      resFile.filename = reqName;
+    }
+    if (info.mimeType) {
+      resFile.mimetype = info.mimeType;
+    }
+    if (info.filename) {
+      resFile.originalname = info.filename;
+    }
+
+    file.on("data", (data) => {
+      totalSize += data.length;
+      resFile.size = totalSize; // Update the size as data comes in
+    });
+
+    file.pipe(fs.createWriteStream(saveTo));
+  });
+
+  bb.on("close", () => {
+    res.writeHead(200, { Connection: "close" });
+    // res.end(`That's all folks!`);
+
+    res.end(
+      JSON.stringify(
+        {
+          message: "File uploaded successfully!",
+          file: resFile,
+        },
+        null,
+        2
+      )
+    );
+  });
+  req.pipe(bb);
 });
 
 // Start the server
